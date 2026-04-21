99e54142626760cefa368b66f69e0a232558cf59
Dany-py <danydegbe@gmail.com>
2026-04-21 22:29:45 +0000

    Update base url

--- Census/src/urls.py
+++ Census/src/urls.py
@@ -24,11 +24,11 @@ from .views import get_csrf_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('csrf/', get_csrf_token, name='get_csrf_token'),
-    path('auth/', include('social_django.urls', namespace='social')),
-    path('users/', include('users.urls', namespace='user')),
-    path('projects/', include('projects.urls', namespace='project')),
-    path('notification/', include('notification.urls', namespace='notification'))
+    path('api/csrf/', get_csrf_token, name='get_csrf_token'),
+    path('api/auth/', include('social_django.urls', namespace='social')),
+    path('api/users/', include('users.urls', namespace='user')),
+    path('api/projects/', include('projects.urls', namespace='project')),
+    path('api/notification/', include('notification.urls', namespace='notification'))
 ]
 
 if settings.DEBUG:
