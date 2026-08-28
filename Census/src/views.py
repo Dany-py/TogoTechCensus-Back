13c708bdd38d25f4a4f8aed1e1ee11e42ca6029d
Dany-py <danydegbe@gmail.com>
2026-08-28 10:45:27 +0000

    Add revival prog

--- Census/src/views.py
+++ Census/src/views.py
@@ -7,3 +7,6 @@ from django.middleware.csrf import get_token
 @ensure_csrf_cookie
 def get_csrf_token(request):
     return JsonResponse({'message': 'csrfToken set.'})
+
+def home(request):
+    return JsonResponse({'message': 'Welcome to TogoTechCensus!'})
